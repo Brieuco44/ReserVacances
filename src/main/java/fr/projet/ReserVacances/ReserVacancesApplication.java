b1a055779e0a6b5124e1511ce73b1a02afa3362a
Brieuc C <brieuc.courapie@free.fr>
2024-10-03 09:35:05 +0200

    first commit

---
+++ src/main/java/fr/projet/ReserVacances/ReserVacancesApplication.java
@@ -0,0 +1,13 @@
+package fr.projet.ReserVacances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserVacancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserVacancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/projet/ReserVacances/ReserVacancesApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.projet.ReserVacances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserVacancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
